cbc16b96de084b2b64f86fefddbcf656e94d614a
chromewhip <connorsteele604@gmail.com>
2020-03-04 13:23:23 -0800

    tablet view complete

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,15 @@ import React, { Component } from "react";
 import "./styles/main.css";
 // import Header from "./components/Header";
 // import Inventory from "./components/Inventory";
-// import Warehouses from "./components/Warehouses";
+import Warehouses from "./components/Warehouses";
 // import Product from "./components/Product";
 
 export default class App extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Warehouses />
+      </div>
+    );
   }
 }
