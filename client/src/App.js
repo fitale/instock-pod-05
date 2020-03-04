8dbca53d778580b418fb55b34ca95c45c60faa1a
Neil Solon <neilsolon@Helens-iPhone.localdomain>
2020-03-03 17:37:02 -0800

    product component mobile 90% done

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,15 @@ import "./styles/main.css";
 import Header from "./components/Header";
 import Inventory from "./components/Inventory";
 import Warehouses from "./components/Warehouses";
+import Product from "./components/Product";
 
 export default class App extends Component {
   render() {
     return (
       <div>
         hey guyz
+        <Header />
+        <Product />
       </div>
     );
   }
