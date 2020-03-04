db442b8b70f8c3da94e4ed8d9fd91edc0fec6a87
fitale <fitalewari@gmail.com>
2020-03-04 13:08:22 -0800

    .get inventory working on backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from "react";
 import "./styles/main.css";
-import Header from "./components/Header";
-import Inventory from "./components/Inventory";
-import Warehouses from "./components/Warehouses";
-import Product from "./components/Product";
+// import Header from "./components/Header";
+// import Inventory from "./components/Inventory";
+// import Warehouses from "./components/Warehouses";
+// import Product from "./components/Product";
 
 export default class App extends Component {
   render() {
-
     return <div></div>;
-
   }
 }
