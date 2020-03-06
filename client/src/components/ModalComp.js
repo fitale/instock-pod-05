290424d5d8ccfb8260cde85dc0123fbd8f3e9f41
chromewhip <connorsteele604@gmail.com>
2020-03-05 17:07:47 -0800

    working post

--- client/src/components/ModalComp.js
+++ client/src/components/ModalComp.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
+import axios from "axios";
 export default class Modal extends Component {
   constructor(props) {
     super(props);
@@ -14,35 +14,117 @@ export default class Modal extends Component {
     this.orderedBy = React.createRef();
     this.referenceNumber = React.createRef();
     this.categories = React.createRef();
+    this.handleUploadSubmit = this.handleUploadSubmit.bind(this);
+  }
+
+  async uploadAProduct() {
+    //Create the video object
+    const newInventory = {
+      name: this.name.value,
+      description: this.description.value,
+      lastOrder: this.lastOrder.value,
+      city: this.city.value,
+      country: this.country.value,
+      quantity: this.quantity.value,
+      status: "in-stock",
+      orderedBy: "Mark Saunders",
+      referenceNumber: "JK2020FD7811201",
+      categories: [
+        "Industrial",
+        "Automotive",
+        "Heavy",
+        "Mechanical",
+        "Engineering",
+        "Sales"
+      ]
+    };
+    // console.log(this.categories.value.split(","));
+    console.log("new inventory item: ", newInventory);
+    //use await and async to make sure all the videos are grabbed
+    await axios
+      .post("http://localhost:5000/api/inventory", newInventory)
+      .then(res => {
+        this.setState({
+          inventory: res.data
+        });
+        return this.state;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  //setup the on click event, using async to confirm the upload is done before redirect
+  async handleUploadSubmit(event) {
+    event.preventDefault();
+    await this.uploadAProduct(); //upload the video
+    // const videoRedirectId = this.state.videos.slice(-1).pop()["id"]; // get the ID of the last submitted video
+    alert("video posted!");
+    // await window.location.replace("/videos/" + videoRedirectId); //redirect to the last posted video
   }
   render() {
     return (
-      <div className="create-new">
+      <form onSubmit={this.handleUploadSubmit} className="create-new">
         <h1 className="create-new__title">Create New</h1>
         <div className="create-new__container">
           <div className="create-new__container--flex">
             <h5 className="title">PRODUCT</h5>
-            <input type="text" placeholder="Item Name" className="input" />
+            <input
+              ref={name => {
+                this.name = name;
+              }}
+              // ref={(this.name = input)}
+              type="text"
+              placeholder="Item Name"
+              className="input"
+            />
           </div>
           <div className="create-new__container--flex">
             <h5 className="title">LAST ORDERED</h5>
-            <input type="text" placeholder="yyyy-mm-dd" className="input" />
+            <input
+              ref={lastOrder => {
+                this.lastOrder = lastOrder;
+              }}
+              type="text"
+              placeholder="yyyy-mm-dd"
+              className="input"
+            />
           </div>
         </div>
         <div className="create-new__container">
           <div className="create-new__container--flex">
             <h5 className="title">CITY</h5>
-            <input type="text" placeholder="City" className="input" />
+            <input
+              ref={city => {
+                this.city = city;
+              }}
+              type="text"
+              placeholder="City"
+              className="input"
+            />
           </div>
           <div className="create-new__container--flex">
             <h5 className="title">COUNTRY</h5>
-            <input type="text" placeholder="Dropdown" className="input" />
+            <input
+              ref={country => {
+                this.country = country;
+              }}
+              type="text"
+              placeholder="Dropdown"
+              className="input"
+            />
           </div>
         </div>
         <div className="create-new__container">
           <div className="create-new__container--flex">
             <h5 className="title">QUANTITY</h5>
-            <input type="text" placeholder="0" className="input" />
+            <input
+              ref={quantity => {
+                this.quantity = quantity;
+              }}
+              type="text"
+              placeholder="0"
+              className="input"
+            />
           </div>
           <div className="create-new__container--flex">
             <h5 className="title">STATUS</h5>
@@ -50,14 +132,17 @@ export default class Modal extends Component {
           </div>
         </div>
         <div className="create-new__container">
-          <div className="create-new__container--description">
-            <h5 className="description">ITEM DESCRIPTION</h5>
-            <input
-              type="text"
-              placeholder="(Optional)"
-              className="create-new__container--input"
-            />
-          </div>
+          <h5 className="create-new__container--description">
+            ITEM DESCRIPTION
+          </h5>
+          <input
+            ref={description => {
+              this.description = description;
+            }}
+            type="text"
+            placeholder="(Optional)"
+            className="create-new__container--input"
+          />
         </div>
         <div className="create-new__container--button">
           <button className="save">SAVE</button>
@@ -68,7 +153,7 @@ export default class Modal extends Component {
             CANCEL
           </button>
         </div>
-      </div>
+      </form>
     );
   }
 }
--- server/routes/api/inventory.js
+++ server/routes/api/inventory.js
@@ -4,6 +4,30 @@ const inventory = require(inventoryFile);
 const helper = require("../../helper/helper");
 const router = express.Router();
 
+router.post("/", (req, res) => {
+  const newInventory = {
+    id: helper.getNewId(),
+    name: req.body.name,
+    description: req.body.description,
+    lastOrder: req.body.lastOrder,
+    city: req.body.city,
+    country: req.body.country,
+    quantity: req.body.quantity,
+    status: req.body.status,
+    orderedBy: req.body.orderedBy,
+    referenceNumber: "JK2020FD7811201",
+    categories: req.body.categories
+  };
+  if (!newInventory.name || !newInventory.description) {
+    return res.status(400).json({
+      errorMessage: "Please provide all the required fields."
+    });
+  }
+  console.log(newInventory);
+  inventory.push(newInventory);
+  helper.writeJSONFile(inventoryFile, inventory);
+  res.json(inventory);
+});
 router.get("/", (req, res) => {
   const inventoryList = inventory.map(item => {
     return {
@@ -32,30 +56,6 @@ router.get("/:id", (req, res) => {
       errorMessage: `Item with ID: ${req.params.id} not found`
     });
   }
-});
-
-router.post("/", (req, res) => {
-  const newInventory = {
-    id: helper.getNewId(),
-    name: req.name,
-    description: req.description,
-    lastOrder: req.lastOrder,
-    city: req.city,
-    country: req.country,
-    quantity: req.quantity,
-    status: req.status,
-    orderedBy: req.orderedBy,
-    referenceNumber: "JK2020FD7811201",
-    categories: req.categories
-  };
-  if (!newInventory.name || !newInventory.description) {
-    return res.status(400).json({
-      errorMessage: "Please provide all the required fields."
-    });
-  }
-  inventorys.push(newInventory);
-  helper.writeJSONFile(inventorysFile, inventorys);
-  res.json(inventorys);
 });
 
 module.exports = router;
